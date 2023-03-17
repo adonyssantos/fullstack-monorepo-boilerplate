214001b56a3f80c025810e61e3df321c16ed6423
Adonys Santos <58638286+adonyssantos@users.noreply.github.com>
2023-03-16 20:16:10 -0400

    Rename app from web-app to web

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <main>Hello world</main>
+      <main>Hello from Next Web App!!</main>
     </>
   );
 }
